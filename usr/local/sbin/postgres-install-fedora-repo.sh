cd4795d2e5abd3ec91c4b3f1cbc3854822e59e4d
The Clansman <jose.churro@gmail.com>
2025-06-28 21:20:13 +0100

    markdown

--- usr/local/sbin/postgres-install-fedora-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# Copyright (c) 2019-2022 Jose Manuel Churro Carvalho
-#
-
-dnf install https://download.postgresql.org/pub/repos/yum/reporpms/F-34-x86_64/pgdg-fedora-repo-latest.noarch.rpm
-
--- usr/local/sbin/postgres-install-redhat-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# Copyright (c) 2019-2022 Jose Manuel Churro Carvalho
-#
-
-dnf install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-
